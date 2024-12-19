ff7d94572b668ce4e8867f34d7dba77780ee5afb
JaViKo500 <javikogutierrez64@gmail.com>
2024-12-18 21:56:05 -0500

    separate html and events

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 const { createApp, ref } = Vue;
 
 const app = createApp({
-   template: `
-      <h1>{{ message }}</h1>
-      <p>Author: {{ author }}</p>
-   `,
+   // template: `
+   //    <h1>{{ message }}</h1>
+   //    <p>Author: {{ author }}</p>
+   // `,
    setup(){
       console.log('<--------------- JK App --------------->');
       console.log('setup');
 
       const message = ref('Hello Vue');
       const author = ref('John');
-      setTimeout(() => {
+      // setTimeout(() => {
+      //    message.value = 'Hello Vue options';
+      //    author.value = 'John Wick';
+      // }, 2000);
+      const changeMessage = () => {
          message.value = 'Hello Vue options';
          author.value = 'John Wick';
-      }, 2000);
-      return { message, author, };
+      };
+      return { message, author, changeMessage};
    }
 });
 
