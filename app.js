509b1da9ef1504885ed59c65c0e825ab42bfeb2f
JaViKo500 <javikogutierrez64@gmail.com>
2024-12-18 21:48:54 -0500

    reactive variables

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-console.log('<--------------- JK App --------------->');
-console.log(Vue);
 const { createApp, ref } = Vue;
 
 const app = createApp({
-   template: '<h1>Hello Vue</h1><p>From app js</p>',
+   template: `
+      <h1>{{ message }}</h1>
+      <p>Author: {{ author }}</p>
+   `,
+   setup(){
+      console.log('<--------------- JK App --------------->');
+      console.log('setup');
+
+      const message = ref('Hello Vue');
+      const author = ref('John');
+      setTimeout(() => {
+         message.value = 'Hello Vue options';
+         author.value = 'John Wick';
+      }, 2000);
+      return { message, author, };
+   }
 });
 
 app.mount('#my-app');
